fef3be2ad1ca2d7776f1f9de80ca88b18a0c2d25
Julio Borges <julioborges05@hotmail.com>
2020-07-21 01:15:38 -0300

    improvement in progress

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,70 +1,56 @@
 let valueNumber = "";
-let expression = "";
-let comand = "";
-let result = 0;
-let value1, value2;
-let count = 1;
+let values = [];
+let index = 0;
+let operator = "";
+let lastOperator = "";
+let result;
 
 function AppComponent() {
-    const [num, alternum] = React.useState(0);
-    const [exp, alterexp] = React.useState(expression);
+    let [num, alternum] = React.useState(0);
+    let [exp, alterExp] = React.useState("");
 
-    const alterExp = function (){
-        if(count == 1){
-            value1 = parseFloat(valueNumber);
-            count++;
-        }else{
-            value2 = parseFloat(valueNumber);
-            count--;
-        }
-
-
-        if(expression[expression.length-1] == "="){
-            if(comand == "mult"){
-                alternum(0);
-                result = mult(value1, value2);
-                alternum(result);
-            } else if(comand == "div"){
-                alternum(0);
-                result = divi(value1, value2);
-                alternum(result);
-            } else if(comand == "sum"){
-                alternum(0);
-                result = sum(value1, value2);
-                alternum(result);
-            } else if(comand == "sub"){
-                alternum(0);
-                result = sub(value1, value2);
-                alternum(result);
-            }
-        }else{
-            if(expression[expression.length-2] == "/"){
-                //console.log("DIVISÃO");
-                comand = "div";
-            }else if(expression[expression.length-2] == "X"){
-                comand = "mult";
-            }else if(expression[expression.length-2] == "+"){
-                comand = "sum";
-            }else if(expression[expression.length-2] == "-"){
-                comand = "sub";
-            }
-            alternum("0");
-        }
-
-
-        alterexp(expression);
-        valueNumber = "";
+    const alterValue = function(){
+        alternum(parseFloat(valueNumber));
+        values[index] = parseFloat(valueNumber);
+        console.log(values);
     }
 
-    const mudaValor = function (){
-        alternum(parseFloat(valueNumber));
+    const alterExpression = function(){
+        "C" != operator ? alterExp(values[index] + operator) : alterExp("");
+        "" == exp ? (0 == index ? index++ : index--): index = 0;
+
+        if ("=" == operator){
+            if ("/" == lastOperator){
+                result = div(values[0], values[1]);
+                alterExp(values[0] + "/" + values[1] + "=");
+                alternum(parseFloat(result));
+            } else if ("X" == lastOperator){
+                result = mult(values[0], values[1]);
+                alterExp(values[0] + "X" + values[1] + "=");
+                alternum(parseFloat(result));
+            } else if ("-" == lastOperator){
+                result = sub(values[0], values[1]);
+                alterExp(values[0] + "-" + values[1] + "=");
+                alternum(parseFloat(result));
+            } else if ("+" == lastOperator){
+                result = sum(values[0], values[1]);
+                alterExp(values[0] + "+" + values[1] + "=");
+                alternum(parseFloat(result));
+            }
+        } else {
+            alternum(0);
+        }
+
+        //console.log(num);
+
+        valueNumber = "";
     }
 
     return(
         <div id="back">
             <History exp={exp}/>
             <Display num={parseFloat(num)}/>
-            <Button muda={mudaValor} exp={alterExp}/>
+            <Button alter={alterValue} alterE={alterExpression}/>
         </div>
     )   
 }
@@ -90,24 +76,24 @@ function Display(props){
 function Button(props){
     return(
         <React.Fragment>
-            <button className="button" onClick = {() => {valueNumber = "0"; props.muda()}}>CE</button>
-            <button className="button" onClick = {() => {expression = ""; props.exp()}}>C</button>
-            <button className="button" onClick = {() => {expression = valueNumber + " / "; props.exp()}}>/</button>
-            <button className="button" onClick = {() => {expression = valueNumber + " X "; props.exp()}}>X</button>
-            <button className="button" onClick = {() => {valueNumber += "7"; props.muda()}}>7</button>
-            <button className="button" onClick = {() => {valueNumber += "8"; props.muda()}}>8</button>
-            <button className="button" onClick = {() => {valueNumber += "9"; props.muda()}}>9</button>
-            <button className="button" onClick = {() => {expression = valueNumber + " - "; props.exp()}}>-</button>
-            <button className="button" onClick = {() => {valueNumber += "4"; props.muda()}}>4</button>
-            <button className="button" onClick = {() => {valueNumber += "5"; props.muda()}}>5</button>
-            <button className="button" onClick = {() => {valueNumber += "6"; props.muda()}}>6</button>
-            <button className="button" onClick = {() => {expression = valueNumber + " + "; props.exp()}}>+</button>
-            <button className="button" onClick = {() => {valueNumber += "1"; props.muda()}}>1</button>
-            <button className="button" onClick = {() => {valueNumber += "2"; props.muda()}}>2</button>
-            <button className="button" onClick = {() => {valueNumber += "3"; props.muda()}}>3</button>
-            <button id="equal" onClick = {() => {expression += valueNumber + " ="; props.exp()}}>=</button>
-            <button id="zero" onClick = {() => {valueNumber += "0"; props.muda()}}>0</button>
-            <button className="button" onClick = {() => {valueNumber += "."; props.muda()}}>.</button>
+            <button className="button" onClick = {() => {valueNumber = "0"; props.alter()}}>CE</button>
+            <button className="button" onClick = {() => {valueNumber = "0"; operator="C"; values = []; index = 0; props.alter(); props.alterE()}}>C</button>
+            <button className="button" onClick = {() => {operator = "/"; props.alterE()}}>/</button>
+            <button className="button" onClick = {() => {operator = "X"; props.alterE()}}>X</button>
+            <button className="button" onClick = {() => {valueNumber += "7"; props.alter()}}>7</button>
+            <button className="button" onClick = {() => {valueNumber += "8"; props.alter()}}>8</button>
+            <button className="button" onClick = {() => {valueNumber += "9"; props.alter()}}>9</button>
+            <button className="button" onClick = {() => {operator = "-"; props.alterE()}}>-</button>
+            <button className="button" onClick = {() => {valueNumber += "4"; props.alter()}}>4</button>
+            <button className="button" onClick = {() => {valueNumber += "5"; props.alter()}}>5</button>
+            <button className="button" onClick = {() => {valueNumber += "6"; props.alter()}}>6</button>
+            <button className="button" onClick = {() => {operator = "+"; props.alterE()}}>+</button>
+            <button className="button" onClick = {() => {valueNumber += "1"; props.alter()}}>1</button>
+            <button className="button" onClick = {() => {valueNumber += "2"; props.alter()}}>2</button>
+            <button className="button" onClick = {() => {valueNumber += "3"; props.alter()}}>3</button>
+            <button id="equal" onClick = {() => {lastOperator = operator; operator = "="; props.alterE()}}>=</button>
+            <button id="zero" onClick = {() => {valueNumber += "0"; props.alter()}}>0</button>
+            <button className="button" onClick = {() => {valueNumber += "."; props.alter()}}>.</button>
         </React.Fragment>
     )
 }
